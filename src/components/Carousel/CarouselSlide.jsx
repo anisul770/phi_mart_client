2f0cd0e2494e91512761ae5a8429ed4ff9fbd5e8
Anisul Haque <anisulhaque773@gmail.com>
2026-01-25 02:04:08 +0100

    react router carousel

---
+++ src/components/Carousel/CarouselSlide.jsx
@@ -0,0 +1,23 @@
+import bg_img from "../../assets/banner-image-bg.jpg"
+
+const CarouselSlide = ({title,subtitle,image}) => {
+  return (
+    <section className="w-full h-[650px] bg-cover bg-center flex justify-center items-center px-4 md:px-8" style={{backgroundImage:`url(${bg_img})`}}>
+      <div className="max-w-6xl flex flex-col gap-8 md:gap-0 md:flex-row items-center justify-between px-8">
+        {/* left content */}
+        <div className="w-full md:w-1/2 text-center md:text-left">
+          <h1 className="text-4xl md:text-5xl font-bold text-gray-900">{title}</h1>
+          <p className="text-gray-600 my-4">{subtitle}</p>
+          <button className="btn btn-secondary px-6 py-3 rounded-full shadow-md">Shop Product</button>
+        </div>
+
+        {/* right content */}
+        <div className="w-full md:w-1/2 flex justify-center">
+          <img className="max-w-full md:max-w-md drop-shadow-lg" src={image} alt="" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CarouselSlide;
---
+++ src/components/Carousel/HeroCarousel.jsx
@@ -0,0 +1,59 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import CarouselSlide from './CarouselSlide';
+import book from "../../assets/book.png";
+
+const HeroCarousel = () => {
+
+  const slides = [
+    {
+      title: "This Fine Print Book Collections",
+      subtitle: "Discount available. Grab it now!",
+      image : book,
+    },
+    {
+      title: "This Fine Print Book Collections",
+      subtitle: "Discount available. Grab it now!",
+      image : book,
+    },
+    {
+      title: "This Fine Print Book Collections",
+      subtitle: "Discount available. Grab it now!",
+      image : book,
+    },
+  ]
+
+  return (
+    <>
+      <Swiper
+        centeredSlides={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {slides.map((slide,index) => (
+          <SwiperSlide key={index}>
+            <CarouselSlide title = {slide.title} subtitle={slide.subtitle} image={slide.image}/>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+};
+
+export default HeroCarousel;
