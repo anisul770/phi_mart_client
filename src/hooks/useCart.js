36e6ebf9063f5a31e75afc63996598885721fd8a
Anisul Haque <anisulhaque773@gmail.com>
2026-02-23 20:32:14 +0100

    product details and cart page

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import apiClient from '../services/api-client';
 import authApiClient from '../services/auth-api-client';
 
@@ -6,8 +6,10 @@ const useCart = () => {
   const [authToken] = useState(() => JSON.parse(localStorage.getItem("authTokens"))?.access);
   const [cart, setCart] = useState(null);
   const [cartId, setCartId] = useState(() => localStorage.getItem("cartId"));
+  const [loading, setLoading] = useState(false);
   // Create a new cart
-  const createOrGetCart = async () => {
+  const createOrGetCart = useCallback(async () => {
+    setLoading(true);
     try {
       const response = await authApiClient.post("/carts/");
       if (!cartId) {
@@ -17,28 +19,56 @@ const useCart = () => {
       setCart(response.data);
     } catch (error) {
       console.log(error.response);
+    } finally {
+      setLoading(false);
     }
-  }
+  }, [authToken, cartId]);
 
   // Add items to the cart
-  const AddCartItems = async (product, quantity) => {
+  const AddCartItems = useCallback(async (product, quantity) => {
+    setLoading(true);
     if (!cartId) await createOrGetCart();
     try {
       const response = await apiClient.post(
         `/carts/${cartId}/items/`,
         { product, quantity },
-        {
-          headers: {
-            Authorization: `JWT ${authToken}`
-          },
-        }
       );
       return response.data;
     } catch (error) {
       console.log("Error adding items", error.response.data);
+    } finally {
+      setLoading(false);
     }
-  }
-  return { cart, createOrGetCart, AddCartItems };
+  },[cartId,createOrGetCart]);
+
+  // Update Item Quantity
+  const updateCartItemQuantity = useCallback(async (itemId, quantity)=>{
+    try{
+      await authApiClient.patch(`/carts/${cartId}/items/${itemId}/`,{quantity,});
+    } catch(error){
+      console.log("error updating cart items", error);
+    }
+  },[cartId])
+
+  // Delete Cart Items
+  const deleteCartItems = useCallback(async(itemId) => {
+    try{
+      await authApiClient.delete(`/carts/${cartId}/items/${itemId}/`)
+    }catch(error){
+      console.log(error);
+    }
+  },[cartId])
+
+  useEffect(()=>{
+    const initializeCart = async() => {
+      setLoading(true);
+      await createOrGetCart();
+      setLoading(false);
+    }
+    initializeCart();
+  },[createOrGetCart]);
+
+  return { cart,loading, createOrGetCart, AddCartItems , updateCartItemQuantity, deleteCartItems};
 };
 
 export default useCart;       
