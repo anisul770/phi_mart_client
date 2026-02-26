0b6bd521fc632fac2a5571cbc3abca247495068a
Anisul Haque <anisulhaque773@gmail.com>
2026-02-26 16:35:53 +0100

    profile link

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -80,10 +80,10 @@ const Navbar = () => {
                 tabIndex="-1"
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                 <li>
-                  <a className="justify-between">
+                  <Link to={"/dashboard/profile"} className="justify-between">
                     Profile
                     <span className="badge">New</span>
-                  </a>
+                  </Link>
                 </li>
                 <li><a>Settings</a></li>
                 <li><a onClick={logoutUser}>Logout</a></li>
